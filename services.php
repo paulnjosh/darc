18ff05f69a2250747eee07e9e6e15822de41e4a2
paulnjosh <94949205+paulnjosh@users.noreply.github.com>
2022-01-02 14:05:19 +0300

    Add files via upload

---
+++ services.php
@@ -0,0 +1,152 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-sacle=1">
+  <title>DARC</title>
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <link rel="stylesheet" href="css/fixed.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+  
+  <script src="jquery-3.4.1.js"></script>
+  <script src="bootstrap.min.js"></script>
+</head>
+
+<body>
+
+	<nav  class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+	  <a class="navbar-brand" href="#">HII<!--<img src="images/logo.png">--></a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  
+	  <div class="collapse navbar-collapse" id="navbarResponsive">
+	    <ul class="navbar-nav ml-auto">
+		  <li class="nav-item">
+		    <a class="nav-link" href="#home">Home</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#course">Course</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#features">Features</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#resources">Resources</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#clients">Clients</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#c ontacts">Contacts</a>
+		  </li>
+		</ul>
+	  </div>
+	</nav>
+
+	<div class="container-fluid all-services">
+		<div class="row">
+			<div class="col-md-12 services-main-header">
+				<h2>Our Services</h2>
+			</div>
+
+			<div class="col-md-4">
+				<div class="service">
+					<div class="service-header">
+						<div class="icon">
+							<span class="" ><i class="las la-igloo"></i></span>
+						</div>
+						<h3>Research and Project Scoping</h3>
+						<div class="heading-underline-s"></div>
+					</div>
+					<div class="service-body">
+						<p>We conduct in-depth research for all your business needs. Our innovative and seasoned group of research professionals will conduct a feasibility study which is the baseline for any research prior to the fieldwork. This will ensure that we collect the ideally desired data for the project.</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-md-4">
+				<div class="service">
+					<div class="service-header">
+						<div class="icon">
+							<span class="" ><i class="las la-clipboard-list"></i></span>
+						</div>
+						<h3>Data Collection and Surveys</h3>
+						<div class="heading-underline-s"></div>
+					</div>
+					<div class="service-body">
+						<p>Our qualified experts will engage in the actual data collection stage and surveys. We perform this exercise with the utmost care to ensure that every piece of information collected is of high quality. </p>
+					</div>
+				</div>
+			</div>
+
+			
+			<div class="col-md-4">
+				<div class="service">
+					<div class="service-header">
+						<div class="icon">
+							<span class="" ><i class="las la-chart-area"></i></span>
+						</div>
+						<h3>Reporting Results and Post Implementation</h3>
+						<div class="heading-underline-s"></div>
+					</div>
+					<div class="service-body">
+						<p>After performing data analytics, we present the findings to our clients in a systematic manner. We offer valuable advice on how the companies should utilize the information in order to achieve their goals more easily.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="service">
+					<div class="icon">
+							<span class="" ><i class="lar la-chart-bar"></i></span>
+						</div>
+					<div class="service-header">
+						<h3>Data Analytics</h3>
+						<div class="heading-underline-s"></div>
+					</div>
+					<div class="service-body">
+						<p>At DARC, we are committed to walking with our clients in all steps to ensure that together we obtain valuable insights that can assist the business to progress well. We, therefore, analyze the data collected on behalf of our clients using our unbeatable modern high tech software’s. We draw valid conclusions from the analysis results, which are very vital in assisting businesses in decision making</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-md-6">
+				<div class="service">
+					<div class="service-header">
+						<div class="icon">
+							<span class="" ><i class="las la-laptop-code"></i></span>
+						</div>
+						<h3>Data Science Training</h3>
+						<div class="heading-underline-s"></div>
+					</div>
+					<div class="service-body">
+						<p>At DARC, we also train interested individuals who want to learn more about data science through our “DARC Data Science Project” program. We have three different cycles throughout the year, and students can enrol any time before a particular cycle begins. We teach different data analysis software, including R, R-Studio, SPSS, STATA, Minitab, E-Views, Medicalc, Advanced EXCEL, and other software. We provide certifications after successful completion of the course, which takes four weeks. We have both Zoom online classes and physical classes. To learn more about our data science programs and classes, .</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		 <!----Start Contact section------>
+  <div id="contact" class="offset">
+    <footer>
+	  <div class="row justify-content-center">
+	    <div class="col-md-5 text-center">
+		  
+		  <p>(888) 888-8888<br>email@dark.com</p>
+		  <a href="#" target="_blank"><i class="lab la-facebook"></i></i></a>
+		  <a href="#" target="_blank"><i class="lab la-twitter"></i></i></a>
+		  <a href="#" target="_blank"><i class="lab la-instagram"></i></i></a>
+		</div>
+		<hr class="socket">
+		&copy;dark.
+	  </div>
+	</footer>
+  </div>
+  <!------End Contact section------>
+	</div>
+</body>
+</html>
